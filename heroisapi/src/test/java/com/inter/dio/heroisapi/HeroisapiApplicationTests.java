fd63bb618a58669265d5e5da394feee0bc14e013
Bruno Colt <38595972+Brcolt@users.noreply.github.com>
2021-04-19 15:21:04 -0300

    Adicionando as configurações iniciais

---
+++ heroisapi/src/test/java/com/inter/dio/heroisapi/HeroisapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inter.dio.heroisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroisapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
